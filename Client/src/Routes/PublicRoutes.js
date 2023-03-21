72897636ad90f53b727e23627bc03b4834cbf457
HasanC14 <chowdhuryh999@gmail.com>
2023-03-21 21:07:04 +0600

    pages added

---
+++ Client/src/Routes/PublicRoutes.js
@@ -0,0 +1,23 @@
+import { createBrowserRouter } from "react-router-dom";
+import Error from "../Components/Error/Error";
+import Home from "../Components/Home/Home";
+import Login from "../Components/Login/Login";
+import Register from "../Components/Register/Register";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/Login", element: <Login></Login> },
+      { path: "/Register", element: <Register></Register> },
+    ],
+  },
+  {
+    path: "*",
+    element: <Error></Error>,
+  },
+]);
+export default router;
---
+++ Client/src/Routes/RiderRoute.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RiderRoute = () => {
+  return <div></div>;
+};
+
+export default RiderRoute;
